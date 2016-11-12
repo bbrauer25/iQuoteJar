82098f8ff2ee99220b5fca1abf9af545ff633d4f
BRAUER, BOBBY [AG/1155] <bbbrau@monsanto.com>
2016-11-12 05:54:26 -0600

    Initial Commit

---
+++ iQuoteJarTests/iQuoteJarTests.swift
@@ -0,0 +1,36 @@
+//
+//  iQuoteJarTests.swift
+//  iQuoteJarTests
+//
+//  Created by BRAUER, BOBBY [AG/1155] on 11/12/2016.
+//  Copyright © 2016 BRAUER, BOBBY [AG/1155]. All rights reserved.
+//
+
+import XCTest
+@testable import iQuoteJar
+
+class iQuoteJarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
